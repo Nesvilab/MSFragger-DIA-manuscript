010fa0910cc958f40f6a50a110b5651c2b5660ba
Fengchao <fcyucn@gmail.com>
2023-03-10 21:31:09 -0500

    Add precursor level table for the iq maxlfq.

--- diann_maxlfq.R
+++ diann_maxlfq.R
@@ -40,6 +40,34 @@ for (path in pathes) {
     runSpecificProteinFDR <- 0.01
 
     if (str_detect(path, "ccRCC") == FALSE) {
+      out_path <- str_c(dirname(path), "/precursor_maxlfq.tsv")
+      if (file.exists(out_path) == FALSE) {
+        print(out_path)
+        print(str_c("global precursor FDR = ", globalPrecursorFDR))
+        print(str_c("global protein FDR = ", globalProteinFDR))
+        print(str_c("run specific precursor FDR = ", runSpecificPrecursorFDR))
+        print(str_c("run specific protein FDR = ", runSpecificProteinFDR))
+
+        df <- fast_read(path,
+                        sample_id = "Run",
+                        primary_id = "Precursor.Id",
+                        secondary_id = "Precursor.Id",
+                        intensity_col = "Precursor.Normalised",
+                        annotation_col = NULL,
+                        filter_string_equal = NULL,
+                        filter_string_not_equal = NULL,
+                        filter_double_less = c("Global.Q.Value" = globalPrecursorFDR, "Global.PG.Q.Value" = globalProteinFDR, "Q.Value" = runSpecificPrecursorFDR, "PG.Q.Value" = runSpecificProteinFDR),
+                        filter_double_greater = NULL,
+                        intensity_col_sep = NULL,
+                        intensity_col_id = NULL,
+                        na_string = "0")
+        df_norm <- fast_preprocess(df$quant_table, median_normalization = FALSE, log2_intensity_cutoff = 0, pdf_out = NULL) # Do not enable median normalization because the tools have their own normalization algorithm.
+        df_maxlfq <- fast_MaxLFQ(df_norm, row_names = df$protein[, 1], col_names = df$sample) # MaxLFQ requires a precursor having non-zero intensities in at least two runs, which makes the "quantified" entries slightly fewer than those from the DIA-NN main report.
+        maxlfq <- df_maxlfq$estimate
+        maxlfq[maxlfq <= 0] <- NA
+        write.table(2^maxlfq, out_path, quote = FALSE, sep = "\t", col.names = NA)
+      }
+
       out_path <- str_c(dirname(path), "/modified_sequence_maxlfq.tsv")
       if (file.exists(out_path) == FALSE) {
         print(out_path)
